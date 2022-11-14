35f78f32cfb663ff60798d65d8e4818401a4ba8f
jackcomey <jack.comey93@gmail.com>
2022-11-14 17:22:46 +0000

    Add questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,37 +76,97 @@ function clearStatusClass(element) {
   element.classList.remove('wrong')
 }
 
+
+
 const questions = [
   {
-    question: 'What is 2 + 2?',
+    question: 'Which is the first book in the series?',
     answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
+      { text: 'Prisoner of Azkaban', correct: false },
+      { text: 'Chamber of Secrets', correct: false },
+      { text: 'Philosophers Stone', correct: true },
+      { text: 'Half Blood Price', correct: false }
     ]
   },
   {
-    question: 'Who is the best YouTuber?',
+    question: 'What is Harrys Mothers Name?',
     answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
+      { text: 'Lily', correct: true },
+      { text: 'Lucy', correct: false },
+      { text: 'Laila', correct: false },
+      { text: 'Lillian', correct: false }
     ]
   },
   {
-    question: 'Is web development fun?',
+    question: 'Who is the eldest Weasley brother?',
     answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
+      { text: 'Percy', correct: false },
+      { text: 'Bill', correct: true },
+      { text: 'George', correct: false },
+      { text: 'Charlie', correct: false }
     ]
   },
   {
-    question: 'What is 4 * 2?',
+    question: 'When is Harrys Birthday?',
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { text: 'October 31st', correct: false },
+      { text: 'November 9th', correct: false },
+      { text: 'July 31st', correct: true },
+      { text: 'April 1st', correct: false }
+    ]
+  },
+  {
+    question: 'Who does Ireland play in the Quidditch World Cup?',
+    answers: [
+      { text: 'Finland', correct: false },
+      { text: 'Kenya', correct: false },
+      { text: 'Vietnam', correct: false },
+      { text: 'Bulgaria', correct: true }
+    ]
+  },
+  {
+    question: 'What happens to Professor Lockhart at the end of the second book?',
+    answers: [
+      { text: 'He is killed by a basilisk', correct: false },
+      { text: 'He joins forces with Voldemort', correct: false },
+      { text: 'He loses his memory', correct: true },
+      { text: 'He becomes headmaster', correct: false }
+    ]
+  },
+  {
+    question: 'Which subject does Hermione get an E in in her OWLS?',
+    answers: [
+      { text: 'Potions', correct: false },
+      { text: 'Defence against the dark arts', correct: true },
+      { text: 'Herbology', correct: false },
+      { text: 'Ancient Ruins', correct: false }
+    ]
+  },
+  {
+    question: 'Who betrays Dumbledores Army?',
+    answers: [
+      { text: 'Marietta Edgecomb', correct: true },
+      { text: 'Cho Chang', correct: false },
+      { text: 'Michael Corner', correct: false },
+      { text: 'Anthony Goldstein', correct: false }
+    ]
+  },
+  {
+    question: 'Who is Firenze?',
+    answers: [
+      { text: 'A Goblin', correct: false },
+      { text: 'A Vampire', correct: false },
+      { text: 'A House Elf', correct: false },
+      { text: 'A Centaur', correct: true }
+    ]
+  },
+  {
+    question: 'Which pet is half kneazle?',
+    answers: [
+      { text: 'Hedwig', correct: false },
+      { text: 'Trevor the toad', correct: false },
+      { text: 'Crookshanks', correct: true },
+      { text: 'Scabbers', correct: false }
     ]
   }
 ]
