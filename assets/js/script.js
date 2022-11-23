985002d33b17713a0d0066f84c36f9124e2fb947
jackcomey <jack.comey93@gmail.com>
2022-11-23 20:40:51 +0000

    Remove final score details from container

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
-const playerScore = document.getElementById('final-score')
+
 
 
 let shuffledQuestions, currentQuestionIndex, score;
