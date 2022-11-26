dc267e0cea84fabf8d921d2ea975464484b2c66f
jackcomey <jack.comey93@gmail.com>
2022-11-26 15:20:48 +0000

    Updating javascript to track current question & increment score when answers are right & tally final score after the last questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,123 @@
 // Declare constants for DOM Elemenets
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const scoreQuestionDiv = document.getElementById('score-live-question');
+const scoreId = document.getElementById('scoreId');
+const liveQuestionId = document.getElementById('liveQuestionId');
 
+let shuffledQuestions, currentQuestionIndex, score, currentQuestionLive, scoreAdded;
 
-
-let shuffledQuestions, currentQuestionIndex, score;
-
-startButton.addEventListener('click', startGame)
+//Initializing Button Clicks
+startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
+//Function to Start the Quiz
 function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
+  scoreQuestionDiv.style.display = 'table';
+  document.getElementById('finalScoreDiv').style.display = "none";
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  currentQuestionLive = 0;
   score = 0;
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  scoreAdded = 0;
+  scoreId.innerText = score+"/10";
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
 }
 
+//Function to view Next question
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+  currentQuestionLive++;
 
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
+  liveQuestionId.innerText = (currentQuestionLive)+"/10";
 
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  if(scoreAdded === 1){
+    scoreAdded = 0;
   }
 }
 
+
+//Function to show Question
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+//Function to reset the state of Quiz 
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+//Checking the answer on the base of clicked button
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct,)
+  const selectedButton = e.target;
+    if(selectedButton.dataset.correct == 'true' && scoreAdded === 0){
+      scoreAdded = 1;
+      score++;
+      scoreId.innerText = score+"/10";
+    }else{
+      scoreAdded = 1;
+    }
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct,);
   Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+    setStatusClass(button, button.dataset.correct);
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove('hide');
   } else {
     gameOver();
   }
 }
 
+//When quiz reached at the end of questions
 function gameOver() {
+  document.getElementById('finalScoreDiv').style.display = "block";
+  document.getElementById('finalScore').innerText = "Your final score is "+score+" out of 10.";
   startButton.innerText = 'Restart';
   startButton.classList.remove('hide');
 }
 
+//Set the colors on the base of selected answers
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
+  clearStatusClass(element);
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add('correct');
   } else {
-    element.classList.add('wrong')
+    element.classList.add('wrong');
   }
 }
 
+//Function to clear the Answers background color
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
 }
 
 
-
+//Questions List which used to display in the Quiz app
 const questions = [
   {
     question: 'Which is the first book in the series?',
@@ -177,4 +209,4 @@ const questions = [
       { text: 'Scabbers', correct: false }
     ]
   }
-]
+];
