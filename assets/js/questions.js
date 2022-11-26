4e13f039e589af07340f879fe9ac07e274577185
jackcomey <jack.comey93@gmail.com>
2022-11-26 16:04:42 +0000

    Create seperate questions.js file and link with script.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,93 @@
+//Questions List which used to display in the Quiz app
+export const questions = [
+  {
+    question: 'Which is the first book in the series?',
+    answers: [
+      { text: 'Prisoner of Azkaban', correct: false },
+      { text: 'Chamber of Secrets', correct: false },
+      { text: 'Philosophers Stone', correct: true },
+      { text: 'Half Blood Price', correct: false }
+    ]
+  },
+  {
+    question: 'What is Harrys Mothers Name?',
+    answers: [
+      { text: 'Lily', correct: true },
+      { text: 'Lucy', correct: false },
+      { text: 'Laila', correct: false },
+      { text: 'Lillian', correct: false }
+    ]
+  },
+  {
+    question: 'Who is the eldest Weasley brother?',
+    answers: [
+      { text: 'Percy', correct: false },
+      { text: 'Bill', correct: true },
+      { text: 'George', correct: false },
+      { text: 'Charlie', correct: false }
+    ]
+  },
+  {
+    question: 'When is Harrys Birthday?',
+    answers: [
+      { text: 'October 31st', correct: false },
+      { text: 'November 9th', correct: false },
+      { text: 'July 31st', correct: true },
+      { text: 'April 1st', correct: false }
+    ]
+  },
+  {
+    question: 'Who does Ireland play in the Quidditch World Cup?',
+    answers: [
+      { text: 'Finland', correct: false },
+      { text: 'Kenya', correct: false },
+      { text: 'Vietnam', correct: false },
+      { text: 'Bulgaria', correct: true }
+    ]
+  },
+  {
+    question: 'What happens to Professor Lockhart at the end of the second book?',
+    answers: [
+      { text: 'He is killed by a basilisk', correct: false },
+      { text: 'He joins forces with Voldemort', correct: false },
+      { text: 'He loses his memory', correct: true },
+      { text: 'He becomes headmaster', correct: false }
+    ]
+  },
+  {
+    question: 'Which subject does Hermione get an E in in her OWLS?',
+    answers: [
+      { text: 'Potions', correct: false },
+      { text: 'Defence against the dark arts', correct: true },
+      { text: 'Herbology', correct: false },
+      { text: 'Ancient Ruins', correct: false }
+    ]
+  },
+  {
+    question: 'Who betrays Dumbledores Army?',
+    answers: [
+      { text: 'Marietta Edgecomb', correct: true },
+      { text: 'Cho Chang', correct: false },
+      { text: 'Michael Corner', correct: false },
+      { text: 'Anthony Goldstein', correct: false }
+    ]
+  },
+  {
+    question: 'Who is Firenze?',
+    answers: [
+      { text: 'A Goblin', correct: false },
+      { text: 'A Vampire', correct: false },
+      { text: 'A House Elf', correct: false },
+      { text: 'A Centaur', correct: true }
+    ]
+  },
+  {
+    question: 'Which pet is half kneazle?',
+    answers: [
+      { text: 'Hedwig', correct: false },
+      { text: 'Trevor the toad', correct: false },
+      { text: 'Crookshanks', correct: true },
+      { text: 'Scabbers', correct: false }
+    ]
+  }
+];
